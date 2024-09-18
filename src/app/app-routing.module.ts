399a8d08a68677cddbf09405a4764bf52e09b946
jhayr311231 <jhayrbuenafe31@gmail.com>
2024-09-18 11:01:35 +0800

    #3 increament by 1 upon click

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { DisplayhelloworldComponent } from './components/displayhelloworld/displayhelloworld.component';
 import { ShowhellobuttonComponent } from './components/showhellobutton/showhellobutton.component';
 import { DisplaynameComponent } from './components/displayname/displayname.component';
+import { CounterComponent } from './components/counter/counter.component';
 
 
 const routes: Routes = [
   { path: 'display-hello-world', component: DisplayhelloworldComponent },
   { path: 'show-hello-button', component: ShowhellobuttonComponent },
   { path: 'display-name', component: DisplaynameComponent },
+  { path: 'counter', component: CounterComponent },
+  
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { DisplayhelloworldComponent } from './components/displayhelloworld/displayhelloworld.component';
 import { ShowhellobuttonComponent } from './components/showhellobutton/showhellobutton.component';
 import { DisplaynameComponent } from './components/displayname/displayname.component';
+import { CounterComponent } from './components/counter/counter.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { DisplaynameComponent } from './components/displayname/displayname.compo
     DisplayhelloworldComponent,
     ShowhellobuttonComponent,
     DisplaynameComponent,
+    CounterComponent,
 
 
 
